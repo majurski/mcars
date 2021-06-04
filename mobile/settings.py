a0b1e58a1aa80df98701c452cb3b337417393e10
minko <majurski@gmail.com>
2021-06-04 18:17:43 +0300

    onother

--- mobile/settings.py
+++ mobile/settings.py
@@ -127,11 +127,11 @@ LOGOUT_REDIRECT_URL = '/cars'
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 
-STATIC_ROOT  =   os.path.join(BASE_DIR, 'static')
+STATIC_ROOT  = os.path.join(BASE_DIR / 'staticfiles')
 STATIC_URL = '/static/'
 
 
-MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT  = os.path.join(BASE_DIR / 'media')
 MEDIA_URL = '/media/'
 
 # Default primary key field type
--- mobile/urls.py
+++ mobile/urls.py
@@ -26,5 +26,9 @@ urlpatterns = [
     path('cars/', include('cars.urls')),
     path('search/', include('search.urls')),
     path('register/', include('register.urls', 'django.contrib.auth.urls')),
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
