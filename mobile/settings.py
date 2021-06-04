6b6f9c97e0bbe04921db8c302151b017c1d09499
minko <majurski@gmail.com>
2021-06-04 17:16:54 +0300

    ano more

--- mobile/settings.py
+++ mobile/settings.py
@@ -130,10 +130,6 @@ PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
 STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    ("static", BASE_DIR / 'static'),
-    ("media", BASE_DIR / 'media'),
-]
 
 MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
